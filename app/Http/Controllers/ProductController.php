e0ab66307e6d2ae480d341daf4738ebb22c0dd8d
Aqui05 <kikissagbeaquilas@gmail.com>
2024-01-31 15:14:58 +0100

    dd commit

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -75,7 +75,7 @@ class ProductController extends Controller
             'quantity' =>'required',
             'categorie_id' => 'required|exists:categories,id',
             'subcategory_id' => 'required|exists:subcategories,id',
-            'photo_path' => 'string|max:2048|image|url'
+            'photo_path' => 'string|max:2048|image'
         ]);
 
 
@@ -147,7 +147,7 @@ class ProductController extends Controller
             'description' => 'string',
             'quantity' =>'required',
             'subcategory_id' => 'required|exists:subcategories,id',
-            'photo_path' => 'string|max:2048|image|url'
+            'photo_path' => 'string|max:2048|image'
         ]);
 
         // Mettez à jour les données du produit
